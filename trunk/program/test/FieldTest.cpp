146f0ac3858ddfbab364133ea4999f04ab467dbf
zdrzalik-przemek <57731778+zdrzalik-przemek@users.noreply.github.com>
2019-12-31 20:04:54 +0100

    Pawn Movement implemented, basic tests

--- trunk/program/test/FieldTest.cpp
+++ trunk/program/test/FieldTest.cpp
@@ -13,13 +13,13 @@ BOOST_AUTO_TEST_SUITE(FieldTestSuite)
 
 BOOST_AUTO_TEST_CASE(FieldTest1) {
     std::shared_ptr<Field> test_field_1 = std::make_shared<Field>(row_1, col_1);
-    std::shared_ptr<Position> test_pos_1 = std::make_shared<Position>(row_1, col_1);
-    BOOST_REQUIRE_EQUAL(test_pos_1->row, row_1);
-    BOOST_REQUIRE_EQUAL(test_pos_1->col, col_1);
-    BOOST_REQUIRE_EQUAL(test_pos_1->row, test_field_1->get_position().row);
-    BOOST_REQUIRE_EQUAL(test_pos_1->col, test_field_1->get_position().col);
-    // TODO: add == operator for Postion
-    //BOOST_REQUIRE_EQUAL(test_pos_1, test_field_1->get_position());
+    Position test_pos_1(row_1, col_1);
+    BOOST_REQUIRE_EQUAL(test_pos_1.row, row_1);
+    BOOST_REQUIRE_EQUAL(test_pos_1.col, col_1);
+    BOOST_REQUIRE_EQUAL(test_pos_1.row, test_field_1->get_position().row);
+    BOOST_REQUIRE_EQUAL(test_pos_1.col, test_field_1->get_position().col);
+    Position tmp = test_field_1->get_position();
+    BOOST_REQUIRE_EQUAL(test_pos_1.operator==(tmp), true);
 
 }
 
